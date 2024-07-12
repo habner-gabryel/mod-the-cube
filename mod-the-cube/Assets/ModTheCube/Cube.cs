060a276cc5add54f9681aeaad0c2c3d9343b8a63
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-12 16:15:31 -0300

    1.4 commit

--- mod-the-cube/Assets/ModTheCube/Cube.cs
+++ mod-the-cube/Assets/ModTheCube/Cube.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
+    public float rangMin = 0.0F;
+    public float rangMax = 180.0F;
+    public float rangeMaxColor = 1F;
     
     void Start()
     {
@@ -13,11 +16,22 @@ public class Cube : MonoBehaviour
         
         Material material = Renderer.material;
         
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        material.color = new Color(0.5f, 6.0f, 0.9f, 0.4f);
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        float rotate = Random.Range(rangMin, rangMax);
+
+        transform.Rotate(rotate * Time.deltaTime, 0.0F, 0.0F);
+
+        Material material = Renderer.material;
+
+        float randomFirst = Random.Range(rangMin, rangeMaxColor);
+        float randomSec = Random.Range(rangMin, rangeMaxColor);
+        float randomThrid = Random.Range(rangMin, rangeMaxColor);
+        float randomFourth = Random.Range(rangMin, rangeMaxColor);
+        
+        material.color = new Color(randomFirst, randomSec, randomThrid, randomFourth);
     }
 }
